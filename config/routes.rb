303192fa07a91af83ad768620fe45a4df34ed763
aatish-puniani <inainup@gmail.com>
2016-04-16 17:58:14 -0500

    installed devise before running devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'photo_assistants#index'
+  get '/photo_assistants' => 'photo_assistants#index'
+  get '/photo_assistants/new' => 'photo_assistants#new'
+  post '/photo_assistants' => 'photo_assistants#create'
+  get '/photo_assistants/:id' => 'photo_assistants#show'
+  get '/photo_assistants/:id/edit' => 'photo_assistants#edit'
+  patch '/photo_assistants/:id' => 'photo_assistants#update'
+  delete '/photo_assistants/:id' => 'photo_assistants#destroy'
 end
