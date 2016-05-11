5dc16d74618c25b96890b9f5aaa0fc50b3cddf29
aatish-puniani <inainup@gmail.com>
2016-05-11 11:57:15 -0500

    created skill model and added name attribute and values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511161240) do
+ActiveRecord::Schema.define(version: 20160511164327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,5 +160,11 @@ ActiveRecord::Schema.define(version: 20160511161240) do
 
   add_index "photographers", ["email"], name: "index_photographers_on_email", unique: true, using: :btree
   add_index "photographers", ["reset_password_token"], name: "index_photographers_on_reset_password_token", unique: true, using: :btree
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
