d2c5a634dd32333713d9090a65fb5da79a127b14
aatish-puniani <inainup@gmail.com>
2016-05-14 14:03:42 -0500

    added navigation for assistant show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :assistant_signed_in?
+
+  def assistant_signed_in?
+    if current_assistant
+      @signed_in_assistant = Assistant.find(current_assistant.id)
+    end
+  end
+
   before_action :photographer_signed_in?
 
   def photographer_signed_in?
